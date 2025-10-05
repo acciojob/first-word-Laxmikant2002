be78b25fa6e07d2ef15780314d6211490973e10c
Laxmikant Shinde <laxmikant251102@gmail.com>
2025-10-05 19:30:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	s = s.trimStart();
+  // Find the first space
+  let idx = s.indexOf(' ');
+  // If no space, return the whole string
+  if (idx === -1) return s;
+  // Otherwise, return up to the first space
+  return s.slice(0, idx);
 }
 
 // Do not change the code below
